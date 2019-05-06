da3150cda5ce8afa9f4a0bc8e0a28a07a23a059c
Siyuan Liu <me@shawnliu.me>
2019-05-06 03:34:29 +0000

    implement the actual filtering

--- sdtui.go
+++ sdtui.go
@@ -50,28 +50,10 @@ func getAllServiceUnits(conn *dbus.Conn) ([]ServiceUnit, error) {
 	return serviceUnits, nil
 }
 
-func main() {
-	filterText := ""
-	app := tview.NewApplication()
-	grid := tview.NewGrid()
-	filterInput := tview.NewInputField()
-	sdUnitList := tview.NewTable()
-	helpText := tview.NewTextView().
-		SetTextAlign(tview.AlignCenter).
-		SetText("(q) Exit (r) Restart service (e) Edit (/) Filter")
-
-	filterInput.SetLabel("Filter by: ").
-		SetDoneFunc(func(key tcell.Key) {
-			if key == tcell.KeyEnter || key == tcell.KeyEscape {
-				filterText = filterInput.GetText()
-				grid.RemoveItem(filterInput).
-					AddItem(helpText, 1, 0, 1, 1, 0, 0, false)
-				app.SetFocus(sdUnitList)
-			}
-		})
-
-	// display services
-	sdUnitList.SetFixed(1, 4).
+func drawTable(table *tview.Table, unitList []ServiceUnit, filter string) {
+	// draw headers
+	table.Clear().
+		SetFixed(1, 4).
 		SetCell(0, 0,
 			tview.NewTableCell("Enabled").
 				SetTextColor(tcell.ColorTeal).
@@ -91,41 +73,73 @@ func main() {
 			tview.NewTableCell("Description").
 				SetTextColor(tcell.ColorTeal).
 				SetAttributes(tcell.AttrBold).
-				SetSelectable(false)).
-		SetSelectable(true, false)
-	sdUnitList.SetBorder(true).
-		SetBorderColor(tcell.ColorGray).
-		SetBorderPadding(0, 0, 1, 1).
-		SetTitle(" sdtui ")
+				SetSelectable(false))
 
+	// draw services
+	index := 0
+	for _, unit := range unitList {
+		if filter == "" || strings.Contains(unit.File.Path, filter) {
+			table.SetCell(index + 1, 0,
+				tview.NewTableCell(unit.File.Type).
+					SetMaxWidth(1).
+					SetExpansion(1))
+			table.SetCell(index + 1, 1,
+				tview.NewTableCell(unit.Status.ActiveState).
+					SetMaxWidth(1).
+					SetExpansion(1))
+			table.SetCell(index + 1, 2,
+				tview.NewTableCell(unit.File.Path).
+					SetMaxWidth(10).
+					SetExpansion(10))
+			table.SetCell(index + 1, 3,
+				tview.NewTableCell(unit.Status.Description).
+					SetMaxWidth(6).
+					SetExpansion(6))
+			index++
+		}
+	}
+}
+
+func main() {
 	dbusConn, err := dbus.New()
 	if err != nil {
 		return
 	}
 	defer dbusConn.Close()
 
-	serviceUnits, err := getAllServiceUnits(dbusConn)
+	allServiceUnits, err := getAllServiceUnits(dbusConn)
 	if err != nil {
 		return
 	}
-	for row, unit := range serviceUnits {
-		sdUnitList.SetCell(row + 1, 0,
-			tview.NewTableCell(unit.File.Type).
-				SetMaxWidth(1).
-				SetExpansion(1))
-		sdUnitList.SetCell(row + 1, 1,
-			tview.NewTableCell(unit.Status.ActiveState).
-				SetMaxWidth(1).
-				SetExpansion(1))
-		sdUnitList.SetCell(row + 1, 2,
-			tview.NewTableCell(unit.File.Path).
-				SetMaxWidth(10).
-				SetExpansion(10))
-		sdUnitList.SetCell(row + 1, 3,
-			tview.NewTableCell(unit.Status.Description).
-				SetMaxWidth(6).
-				SetExpansion(6))
-	}
+
+	filterText := ""
+	app := tview.NewApplication()
+	grid := tview.NewGrid()
+	filterInput := tview.NewInputField()
+	sdUnitList := tview.NewTable()
+	helpText := tview.NewTextView().
+		SetTextAlign(tview.AlignCenter).
+		SetText("(q) Exit (r) Restart (R) Reload (s) Start (S) Stop (e) Enable (d) Disable (/) Filter")
+
+	filterInput.SetLabel("Filter by: ").
+		SetDoneFunc(func(key tcell.Key) {
+			if key == tcell.KeyEnter || key == tcell.KeyEscape {
+				filterText = filterInput.GetText()
+				grid.RemoveItem(filterInput).
+					AddItem(helpText, 1, 0, 1, 1, 0, 0, false)
+				drawTable(sdUnitList, allServiceUnits, filterText)
+				app.SetFocus(sdUnitList)
+			}
+		})
+
+	// display services
+	sdUnitList.SetSelectable(true, false).
+		SetBorder(true).
+		SetBorderColor(tcell.ColorGray).
+		SetBorderPadding(0, 0, 1, 1).
+		SetTitle(" sdtui ")
+
+	drawTable(sdUnitList, allServiceUnits, filterText)
 
 	// define key handler
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
